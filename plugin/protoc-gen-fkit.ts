af535a95c46b7c3189e8c736d9286cc6b486d864
Liam Maguire <liammaguiresoftware@outlook.com>
2023-05-28 11:57:12 +1000

    Gen working.

--- plugin/protoc-gen-fkit.ts
+++ plugin/protoc-gen-fkit.ts
@@ -1,24 +1,43 @@
+#!/usr/bin/env -S npx tsx
+
+// #!/usr/bin/env -S npx tsx is required in addition to chmod 755 on your plugin to get it working locally.
 
 import { createEcmaScriptPlugin, runNodeJs } from "@bufbuild/protoplugin";
 import {
-  literalString,
-  makeJsDoc,
-  localName,
-  findCustomMessageOption,
+    literalString,
+    makeJsDoc,
+    localName,
+    findCustomMessageOption,
 } from "@bufbuild/protoplugin/ecmascript";
 import { DescMessage, DescMethod, DescService, MethodKind, ScalarType } from "@bufbuild/protobuf";
 import type { Schema } from "@bufbuild/protoplugin/ecmascript";
 
-const protocGengooEs = createEcmaScriptPlugin({
+const protocGen = createEcmaScriptPlugin({
     name: "protoc-gen-goo-es",
     version: `v0.2.1`,
     parseOption,
     generateTs,
 });
 
-  function parseOption(key: string, value: string | undefined){
-  
-  }
+runNodeJs(protocGen);
 
-  function generateTs(schema: Schema) {}
-  
+function parseOption(key: string, value: string | undefined) {
+
+}
+
+function generateTs(schema: Schema) {
+    // find all messages within the proto.
+    let i;
+    for (i = 0; i < schema.files.length; i++) {
+        schema.files[i].messages
+    }
+}
+
+function generateCode(schema: Schema, message: DescMessage){
+    // generate message for name.
+    const f = schema.generateFile(`${message.name}.ts`);
+
+    // Generate Views for all.
+    // Generate List, Get, Create, Delete and Update firestore code.
+    f.print("// tuki.")
+}
